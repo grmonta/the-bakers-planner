6a9d526bab70ab9e6904b237db8a0f42e7d80ad1
Grmonta <grmonta@gmail.com>
2019-11-24 20:18:18 -0600

    update style on calc form

--- src/components/recipeCalculator/addStep.js
+++ src/components/recipeCalculator/addStep.js
@@ -4,71 +4,107 @@ import PropTypes from "prop-types"
 import styled from "@emotion/styled"
 import tw from "tailwind.macro"
 import FormLabel from "./formLabel"
-import FormButton from "../shared/formButton"
-const FormInputContainer = styled.div`
+
+const FormContainer = styled.div`
   ${tw`
-  w-full
+  w-full max-w-lg mx-auto
+  `}
+`
+
+const InputGroup = styled.div`
+  ${tw`
+  flex flex-wrap -mx-3 mb-2 items-center 
 `}
 `
+
+const FormInputContainer = styled.div`
+  ${tw`
+  w-full  px-3 mb-6 md:mb-0
+`}
+`
+
+const FormInputHalf = styled.div`
+  ${tw`
+  w-1/2 px-3 mb-6 md:mb-0
+`}
+`
+
 const FormInputStyle = styled.div`
   ${tw` 
-    appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-2 px-2 mb-3 leading-tight  md:mr-2 focus:outline-none focus:bg-white
+    bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500
 
   `}
 `
 
-export const AddStep = ({ name, hours, minutes, onChange, onSubmit }) => (
-  <div className="row justify-content-center">
-    <form onSubmit={onSubmit} className="form-inline">
-      <FormInputContainer>
-        <FormLabel for="step name" labelName="Name of Step" />
-        <FormInputStyle>
-          <input
-            type="text"
-            placeholder="ex: first proof"
-            name="name"
-            value={name}
-            required
-            onChange={onChange}
-          />
-        </FormInputStyle>
-      </FormInputContainer>
-      <FormInputContainer>
-        <FormLabel for="hours input" labelName="Hours*" />
-        <FormInputStyle>
-          <input
-            type="number"
-            min="0"
-            max="1000"
-            required
-            placeholder="0"
-            name="hours"
-            value={hours}
-            onChange={onChange}
-          />
-        </FormInputStyle>
-      </FormInputContainer>
+const ButtonRow = styled.div`
+  ${tw` 
+    w-1/3 mx-auto
 
-      <FormInputContainer>
-        <FormLabel for="minutes input" labelName="Minutes*" />
-        <FormInputStyle>
-          <input
-            type="number"
-            min="0"
-            max="59"
-            required
-            placeholder="0"
-            name="minutes"
-            value={minutes}
-            onChange={onChange}
-          />
-        </FormInputStyle>
-      </FormInputContainer>
-      <FormButton name="Add Step">
-        <button type="submit"></button>
-      </FormButton>
+  `}
+`
+
+const FormButton = styled.div`
+  ${tw`
+        bg-teal-700 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded mr-2 cursor-pointer
+    `}
+`
+
+export const AddStep = ({ name, hours, minutes, onChange, onSubmit }) => (
+  <FormContainer>
+    <form onSubmit={onSubmit} className="form-inline">
+      <InputGroup>
+        <FormInputContainer>
+          <FormLabel for="step name" labelName="Name of Step" />
+          <FormInputStyle>
+            <input
+              type="text"
+              placeholder="ex: first proof"
+              name="name"
+              value={name}
+              required
+              onChange={onChange}
+            />
+          </FormInputStyle>
+        </FormInputContainer>
+        <FormInputHalf>
+          <FormLabel for="hours input" labelName="Hours*" />
+          <FormInputStyle>
+            <input
+              type="number"
+              min="0"
+              max="1000"
+              required
+              placeholder="0"
+              name="hours"
+              value={hours}
+              onChange={onChange}
+            />
+          </FormInputStyle>
+        </FormInputHalf>
+
+        <FormInputHalf>
+          <FormLabel for="minutes input" labelName="Minutes*" />
+          <FormInputStyle>
+            <input
+              type="number"
+              min="0"
+              max="59"
+              required
+              placeholder="0"
+              name="minutes"
+              value={minutes}
+              onChange={onChange}
+            />
+          </FormInputStyle>
+        </FormInputHalf>
+      </InputGroup>
+      <ButtonRow>
+        <FormButton>
+          <button type="submit">Add Step </button>
+        </FormButton>
+      </ButtonRow>
     </form>
-  </div>
+  </FormContainer>
 )
 
 AddStep.propTypes = {
